fd64564fc940e419c67314e9bc6ea87ae986cfc8
gkkangk@gmail.com <gkkangk@gmail.com>
2024-07-01 21:09:16 +0300

    Audio System Progress

---
+++ Assets/Scripts/Runtime/Data/ValueObject/GameSoundData.cs
@@ -0,0 +1,12 @@
+using Runtime.Enums;
+using UnityEngine;
+
+namespace Runtime.Data.ValueObject
+{
+    [System.Serializable]
+    public struct GameSoundData
+    {
+        public GameSoundType gameSoundType;
+        public AudioClip audioClip;
+    }
+}
--- Assets/Scripts/Runtime/Managers/SoundManager.cs
+++ Assets/Scripts/Runtime/Managers/SoundManager.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
+using Runtime.Extensions;
+using Runtime.Managers;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
+public class SoundManager : SingletonMonoBehaviour<SoundManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  [SerializeField] private AudioSource audioSource;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void Awake()
+   {
+       base.Awake();
+       audioSource = GetComponent<AudioSource>();
+   }
+   
+   public void PlaySound(AudioClip clip)
+   {
+       if (clip != null && SettingsManager.Instance.isSoundActive)  audioSource.PlayOneShot(clip);
+   }
+   
 }
