a0acb9b2f2c98d21f2e7d194cb8e4ec57e9fc1f5
gkkangk@gmail.com <gkkangk@gmail.com>
2024-07-03 21:32:14 +0300

    SoundManager Done

--- Assets/Scripts/Runtime/Managers/SoundManager.cs
+++ Assets/Scripts/Runtime/Managers/SoundManager.cs
@@ -1,3 +1,5 @@
+using Runtime.Data.UnityObject;
+using Runtime.Enums;
 using Runtime.Extensions;
 using Runtime.Managers;
 using UnityEngine;
@@ -5,16 +7,21 @@ using UnityEngine;
 public class SoundManager : SingletonMonoBehaviour<SoundManager>
 {
   [SerializeField] private AudioSource audioSource;
-
-   private void Awake()
+  [SerializeField] private CD_GameSound COLLECTION;
+  
+   public void PlaySound(GameSoundType soundType)
    {
-       base.Awake();
-       audioSource = GetComponent<AudioSource>();
+       if (SettingsManager.Instance.isSoundActive)
+       {
+           foreach (var sound in COLLECTION.gameSoundData)
+           {
+               if (soundType == sound.gameSoundType)
+               {
+                   audioSource.clip = sound.audioClip;
+                   audioSource.Play();
+                   break; 
+               }
+           }
+       }
    }
-   
-   public void PlaySound(AudioClip clip)
-   {
-       if (clip != null && SettingsManager.Instance.isSoundActive)  audioSource.PlayOneShot(clip);
-   }
-   
 }
